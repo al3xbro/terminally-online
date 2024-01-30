a8b2c5e21561eac5b646f1d0eb5cd10ba81d6bc7
Alex Bui <alexnbui@gmail.com>
2024-01-30 00:11:54 -0800

    Cleanup + add logout

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -13,17 +13,21 @@ class LoginStatus(Enum):
 f = open('base_headers.json', 'r')
 headers = json.load(f)
 
-# return ticket if MFA is needed, else save token
+# Saves login token and returns a LoginStatus
+# param: email, password, mfa_code?
+# return: LoginStatus
 def login(email: str, password: str, mfa_code: str = None):
 
     # send login request
-    res = requests.post('https://discord.com/api/v9/auth/login', data=json.dumps({
-        'gift_code_sku_id': None,
-        'login': email,
-        'password': password,
-        'login_source': None,
-        'undelete': False,
-    }), headers = headers | {'referer': 'https://discord.com/login'})
+    res = requests.post(url = 'https://discord.com/api/v9/auth/login', 
+                        data = json.dumps({
+                            'gift_code_sku_id': None,
+                            'login': email,
+                            'password': password,
+                            'login_source': None,
+                            'undelete': False,
+                        }), 
+                        headers = headers | {'referer': 'https://discord.com/login'})
 
     # verify ip
     if res.json().get('errors') and res.json().get('errors').get('login').get('_errors')[0].get('code') == 'ACCOUNT_LOGIN_VERIFICATION_EMAIL':
@@ -35,10 +39,12 @@ def login(email: str, password: str, mfa_code: str = None):
     if res.json().get('ticket') and mfa_code is None:
         return LoginStatus.NEED_MFA
     if res.json().get('ticket') and mfa_code is not None:
-        res = requests.post('https://discord.com/api/v9/auth/mfa/totp', data=json.dumps({
-            'code': mfa_code,
-            'ticket': res.json().get('ticket'),
-        }), headers=headers | {'referer': 'https://discord.com/login'})
+        res = requests.post(url = 'https://discord.com/api/v9/auth/mfa/totp', 
+                            data = json.dumps({
+                                'code': mfa_code,
+                                'ticket': res.json().get('ticket'),
+                            }), 
+                            headers = headers | {'referer': 'https://discord.com/login'})
         # invalid MFA code
         if res.json().get('code') == 60008:
             return LoginStatus.FAILED_MFA
@@ -48,3 +54,10 @@ def login(email: str, password: str, mfa_code: str = None):
     # successful login without MFA
     print(res.json().get('token'))
     return LoginStatus.SUCCESS
+
+def logout():
+
+    # send logout request
+    res = requests.post(url = 'https://discord.com/api/v9/auth/logout', 
+                        headers = headers | {'referer': 'https://discord.com/login', 'authorization': ""})
+    print(res.json())
