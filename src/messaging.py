895bd043ff4c3b1a7507a86eef666afdf3f947f7
Alex Bui <112739079+al3xbro@users.noreply.github.com>
2024-01-31 15:42:45 -0800

    Add messaging utils + websocket (#2)
    * Implement get history + send messages
    
    * Add documentation
    
    * Add limit to get message
    
    * Add websocket starter
    
    * Seperate websocket and message methods
    
    * Switch to functional paradigm
    
    * Cleanup + add comments

---
+++ src/messaging.py
@@ -0,0 +1,46 @@
+import requests
+import json
+import websocket
+
+# base headers for all requests. add referer and authorization
+f = open('base_headers.json', 'r')
+headers = json.load(f)
+f.close()
+
+# get login token TODO: move to auth.py and change token storage
+f = open('token.json', 'r')
+token = json.load(f).get('token')
+f.close()
+
+def get_message_history(guild_id: str, channel_id: str, limit: int) -> list:
+    '''Returns a list of messages in the current channel'''
+
+    # send request
+    res = requests.get(url = f'https://discord.com/api/v9/channels/{channel_id}/messages?limit={limit}', 
+                        headers = headers | {
+                           'referer': f'https://discord.com/channels/{guild_id}/{channel_id}',
+                           'authorization': token,
+                        })
+    
+    # return messages TODO: better error handling and documentation
+    try:
+        return list(res.json())
+    except:
+        return []
+
+def send_message(guild_id: str, channel_id: str, content: str):
+    '''Sends a message to channel'''
+
+    # send request
+    res = requests.post(url = f'https://discord.com/api/v9/channels/{channel_id}/messages', 
+                        headers = headers | {
+                            'referer': f'https://discord.com/channels/{guild_id}/{channel_id}',
+                            'authorization': token,
+                        },
+                        data = json.dumps({'content': content})
+                        )
+    
+    # return status TODO: better error handling and documentation
+    if res.status_code == 200:
+        return True
+    return False
---
+++ src/websocket.py
@@ -0,0 +1,58 @@
+import json
+from websockets.sync.client import connect
+from websockets.client import ClientConnection
+import time
+import threading
+
+# get login token TODO: move to auth.py and change token storage
+f = open('token.json', 'r')
+token = json.load(f).get('token')
+f.close()
+
+def __connect_websocket() ->  ClientConnection:
+    """Connects to Discord websocket with user token. Do not invoke."""
+
+    # websocket handshake
+    ws = connect('wss://gateway.discord.gg/?v=9&encoding=json', max_size=999999999)
+    # get hello and heartbeat interval
+    reply = json.loads(ws.recv())
+
+    # start heartbeat thread
+    interval = reply['d']['heartbeat_interval']
+    threading.Thread(target = __heartbeat, args = (interval, ws)).start()
+
+    # identify yourself
+    ws.send(json.dumps({
+        'op': 2,
+        'd': {
+            'token': token,
+            'compress': False,
+            'properties': {
+                '$os': 'Windows',
+                '$browser': 'Chrome',
+                '$device': ''
+            }
+        }
+    }))
+
+    return ws
+
+def __heartbeat(interval, ws):
+    """Heartbeat for websocket connection"""
+
+    # TODO: send accurate d value
+    print("heartbeat")
+    while True:
+        time.sleep(interval / 1000)
+        ws.send(json.dumps({
+            'op': 1,
+            'd': None
+        }))
+        
+def get_websocket() -> ClientConnection:
+    """Returns ClientConnection object"""
+
+    global ws
+    return ws
+
+ws = __connect_websocket()
