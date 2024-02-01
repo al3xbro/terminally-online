0092a6ef78db0085e13f5c8c1f58a8c89f598fb3
Alex Bui <alexnbui@gmail.com>
2024-01-31 20:13:51 -0800

    OOPify

--- src/websocket/connection.py
+++ src/websocket/connection.py
@@ -5,54 +5,58 @@ import threading
 from websockets.sync.client import connect
 from websockets.client import ClientConnection
 
-# get login token TODO: move to auth.py and change token storage
-f = open('token.json', 'r')
-token = json.load(f).get('token')
-f.close()
+class Connection:
 
-def __connect_websocket() ->  ClientConnection:
-    '''Connects to Discord websocket with user token. Do not invoke.'''
+    # static ws object
+    ws = None
 
-    # websocket handshake
-    ws = connect('wss://gateway.discord.gg/?v=9&encoding=json', max_size=999999999)
-    # get hello and heartbeat interval
-    reply = json.loads(ws.recv())
+    @staticmethod
+    def __connect_websocket() ->  ClientConnection:
+        '''Connects to Discord websocket with user token. Do not invoke.'''
 
-    # start heartbeat thread
-    interval = reply['d']['heartbeat_interval']
-    threading.Thread(target = __heartbeat, args = (interval, ws)).start()
+        # websocket handshake
+        ws = connect('wss://gateway.discord.gg/?v=9&encoding=json', max_size=999999999)
+        # get hello and heartbeat interval
+        reply = json.loads(ws.recv())
 
-    # identify yourself
-    ws.send(json.dumps({
-        'op': 2,
-        'd': {
-            'token': token,
-            'compress': False,
-            'properties': {
-                '$os': 'Windows',
-                '$browser': 'Chrome',
-                '$device': ''
-            }
-        }
-    }))
+        # start heartbeat thread
+        interval = reply['d']['heartbeat_interval']
+        threading.Thread(target = Connection.__heartbeat, args = (interval, ws)).start()
 
-    return ws
-
-def __heartbeat(interval, ws):
-    '''Heartbeat for websocket connection. Do not invoke'''
-
-    # TODO: send accurate d value
-    while True:
-        time.sleep(interval / 1000)
+        # identify yourself
         ws.send(json.dumps({
-            'op': 1,
-            'd': None
+            'op': 2,
+            'd': {
+                'token': json.load(open('token.json', 'r')).get('token'),
+                'compress': False,
+                'properties': {
+                    '$os': 'Windows',
+                    '$browser': 'Chrome',
+                    '$device': ''
+                }
+            }
         }))
+
+        return ws
+
+    @staticmethod
+    def __heartbeat(interval, ws):
+        '''Heartbeat for websocket connection. Do not invoke'''
+
+        # TODO: send accurate d value
+        while True:
+            time.sleep(interval / 1000)
+            ws.send(json.dumps({
+                'op': 1,
+                'd': None
+            }))
+            
+    @staticmethod
+    def get_websocket() -> ClientConnection:
+        '''Returns ClientConnection object'''
         
-def get_websocket() -> ClientConnection:
-    '''Returns ClientConnection object'''
-
-    global ws
-    return ws
-
-ws = __connect_websocket()
+        if Connection.ws:
+            return Connection.ws
+        else:
+            Connection.ws = Connection.__connect_websocket()
+            return Connection.ws
