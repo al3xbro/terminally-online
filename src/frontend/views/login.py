d95bb886e4dabf07785cdac97d36faa760b9b335
NicholasLe04 <Nicholasl.le224@gmail.com>
2024-02-02 20:20:14 -0800

    Create frontend views

---
+++ src/frontend/views/login.py
@@ -0,0 +1,13 @@
+import os
+
+from views.view import View
+
+class LoginView(View):
+    def __init__(self):
+        super().__init__()
+
+    def draw(self):
+        pass
+
+    def handle_input(self):
+        pass
