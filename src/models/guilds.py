bfe926721802e9d04ac5ea46b3a113543de9230b
Alex Bui <alexnbui@gmail.com>
2024-02-07 16:20:31 -0800

    Implement guilds

--- src/models/guilds.py
+++ src/models/guilds.py
@@ -1,4 +1,32 @@
-from websocket import listener
+from websocket.listener import Listener
 
 class Guilds:
-    pass
+    
+    guilds = []
+    guild_folders = []
+    private_channels = []
+    
+    @staticmethod
+    def __set_guilds(data: dict) -> None:
+        '''Set the guilds'''
+        
+        Guilds.guilds = data['guilds']
+        Guilds.guild_folders = data['user_settings']['guild_folders']
+        Guilds.private_channels = data['private_channels']
+    
+    def get_guilds() -> list:
+        '''Returns the guilds'''
+        
+        return Guilds.guilds
+    
+    def get_guild_folders() -> list:
+        '''Returns the guild folders'''
+        
+        return Guilds.guild_folders
+    
+    def get_private_channels() -> list:
+        '''Returns the private channels'''
+        
+        return Guilds.private_channels
+    
+    Listener.subscribe_event('READY', __set_guilds)
