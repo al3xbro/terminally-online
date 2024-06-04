ad67782591a035c9bfcda6a2801145af8c6a2d61
Alex Bui <alexnbui@gmail.com>
2024-06-03 23:35:59 -0700

    add timestamp and edited message label

--- src/views/message.py
+++ src/views/message.py
@@ -1,6 +1,7 @@
 from textual.widgets import Label, Static
 from textual.reactive import reactive
 from textual.containers import Horizontal
+from datetime import datetime
 
 class Message(Static):
 
@@ -18,3 +19,5 @@ class Message(Static):
         with Horizontal():
             yield Label(f"{self.message['author']['username']}:", classes='username')
             yield Label(self.message['content'], classes='content')
+            yield (Label(' (edited)', classes='edited') if self.message.get('edited_timestamp') else Label(''))
+            yield Label(datetime.fromisoformat(self.message['timestamp']).strftime('%m-%d-%Y %H:%M:%S'), classes='timestamp')
