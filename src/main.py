afad0f5e495bc63048e7fc22959291f201cf71bc
Alex Bui <alexnbui@gmail.com>
2024-06-04 01:17:00 -0700

    make chat input more responsive with async worker

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 from textual.app import App
 from views.chat.chat import Chat
-from views.input.input import CommandInput
+from views.input.input import ChatInput
 from textual.widgets import Header
 
 class TerminallyOnline(App):
 
     CSS_PATH = 'style.tcss'
-    channel_id = '1089098339540291627'
+    channel_id = '832033238004138006'
 
     def compose(self):
         yield Header()
         yield Chat(self.channel_id)
-        yield CommandInput(self.channel_id)
+        yield ChatInput(self.channel_id)
 
 if __name__ == '__main__':
     app = TerminallyOnline()
--- src/views/input/input.py
+++ src/views/input/input.py
@@ -1,7 +1,8 @@
+from textual import work
 from textual.widgets import Input, Static
 from models.messaging import Messaging
 
-class CommandInput(Input):
+class ChatInput(Input):
 
     channel_id = None
 
@@ -10,7 +11,9 @@ class CommandInput(Input):
         self.channel_id = channel_id
         self.placeholder = 'Enter your message here'
 
+    async def submit_message(self, value):
+        Messaging.send_message(self.channel_id, value)
+
     def on_input_submitted(self):
-        if (Messaging.send_message(self.channel_id, self.value)):
-            print('sent')
+        self.run_worker(self.submit_message(self.value))
         self.value = ''
